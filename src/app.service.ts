401edb118b8bc42aab1f2e83008ab2dde32f0905
yurikgs <yurikgs@outlook.com>
2022-03-29 14:57:32 -0300

    final adjustments

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Olá! Bem Vindo à Api do Clima :)';
+    let message: string = 'Olá! Bem Vindo(a) à Api do Clima! :) -----------------------> Acesse o endpoint "/tempo/Nome da Cidade" para acessar as informações climáticas de sua cidade. Se quiser ver um objeto com essas informações, acesse o endpoint "/tempo/objeto/Nome da Cidade"' 
+    return message
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
+
+const welcome = () => {
+  console.log(`\n ╔══════════════════════════════════════════════════════╗\n ║                                                      ║\n ║       Seja muito bem-vindo à API do Clima!           ║\n ║                                                      ║\n ╚══════════════════════════════════════════════════════╝`);
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  welcome()
 }
 bootstrap();
